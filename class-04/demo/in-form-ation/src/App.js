fc2ec90cfa8927b906589b133105c6ba7fb99c5a
mnfmnfm <mnfmnfm@gmail.com>
2021-03-30 19:55:07 -0700

    basic html forms

--- class-04/demo/in-form-ation/src/App.js
+++ class-04/demo/in-form-ation/src/App.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import Header from './Header.js';
 import './App.css';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: 'user'
+    };
+  }
+  handleFormSubmitted = e => {
+    e.preventDefault();
+    console.log('submitted');
+  }
+  handleUsernameTyped = e => {
+    // e.preventDefault();
+    if(e.target.value.length > 0) {
+      this.setState({
+        username: e.target.value
+      });
+    } else {
+      this.setState({
+        username: 'user'
+      });
+    }
+  }
   render() {
     return (
-      <h1>Hello!</h1>
+      <div>
+        <Header potato={this.state.username}/>
+        <form onSubmit={this.handleFormSubmitted}>
+          <label htmlFor="name">Name</label>
+
+          <input id="name" name="name" type="text" onInput={this.handleUsernameTyped} />
+          <br />
+          <label htmlFor="dob">Date of Birth</label>
+          <input type="date" name="dob" id="dob" />
+          <br />
+          <input type="submit" />
+        </form>
+      </div>
     )
   }
 }
