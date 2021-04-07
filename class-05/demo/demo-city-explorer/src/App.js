2dbbf2e852ce98b6d69b53ce139200666ae0b915
mnfmnfm <mnfmnfm@gmail.com>
2021-04-06 18:20:41 -0700

    day 5 demo

--- class-05/demo/demo-city-explorer/src/App.js
+++ class-05/demo/demo-city-explorer/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import Search from './Search.js';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state={
+      haveWeSearchedYet: false,
+      citySearchedFor: '',
+    };
+  }
+
+  handleShowSearch = () => {
+    this.setState({haveWeSearchedYet: false});
+  }
+
+  handleSearch = (citySearchedFor) => {
+    console.log('searched', citySearchedFor);
+    this.setState({
+      haveWeSearchedYet: true,
+      citySearchedFor: citySearchedFor
+    });
+  }
+  render() {
+    return (
+      <>
+        <h1>City Explorer</h1>
+        {this.state.haveWeSearchedYet ? 
+          <button onClick={this.handleShowSearch}>Search again</button> : 
+          <Search handleSearch={this.handleSearch} />}
+      </>
+    );
+  }
 }
 
 export default App;
