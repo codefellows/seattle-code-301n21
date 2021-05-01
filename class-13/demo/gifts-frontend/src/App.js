cd764b174c92015bc326b440f445e2884fb26e86
mnfmnfm <mnfmnfm@gmail.com>
2021-05-01 11:16:25 -0700

    finish gifts demo

--- class-13/demo/gifts-frontend/src/App.js
+++ class-13/demo/gifts-frontend/src/App.js
@@ -7,16 +7,84 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      gifts: []
+      description: '',
+      email: '',
+      gifts: [],
+      name: ''
     }
   }
+  handleEmailInput = (e) => {
+    this.setState({email: e.target.value});
+  }
+  handleNameInput = (e) => {
+    this.setState({name: e.target.value});
+  }
+  handleDescriptionInput = (e) => {
+    this.setState({description: e.target.value});
+  }
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    this.fetchUserData();
+  }
+
+  fetchUserData = () => {
+    axios.get(`${process.env.REACT_APP_SERVER_URL}/users/${this.state.email}`)
+    .then(serverResponse => {
+      console.log(serverResponse.data);
+      this.setState({
+        gifts: serverResponse.data[0].gifts
+      })
+    });
+  }
+
+  handleCreateGift = (e) => {
+    e.preventDefault();
+    console.log('name', this.state.name, 'email', this.state.email, 'description', this.state.description);
+    // make the request to the server with the info the user typed in
+    axios.post(`${process.env.REACT_APP_SERVER_URL}/gifts`, {
+      description: this.state.description,
+      email: this.state.email,
+      name: this.state.name
+    }).then( response => {
+      console.log(response.data);
+      this.setState({
+        gifts: response.data
+      })
+    });
+  }
+
+  handleDelete = (id) => {
+    axios.delete(`${process.env.REACT_APP_SERVER_URL}/gifts/${id}?user=${this.state.email}`).then(responseData => {
+      this.setState({ 
+        gifts: responseData.data
+      })
+    })
+  }
   componentDidMount() {
-    axios.get('http://localhost:3001/')
-    .then(data => console.log(data));
+    axios.get(`${process.env.REACT_APP_SERVER_URL}/`)
+    .then(serverResponse => console.log(serverResponse.data));
   }
   render() {
     return <>
       <h1>Gifts!</h1>
+      <form onSubmit={this.handleFormSubmit}>
+        <input type="text" onInput={this.handleEmailInput} />
+        <input type="submit" />
+      </form>
+      {this.state.gifts.length > 0 && <ul>
+        {this.state.gifts.map(gift => 
+          <li key={gift._id}>{gift.name}: {gift.description} <button onClick={e => this.handleDelete(gift._id)} >Delete</button></li>
+        )}
+        </ul>}
+      <form onSubmit={this.handleCreateGift}>
+        <label htmlFor="name">Name</label>
+        <input id="name" type="text" onInput={this.handleNameInput}></input>
+        <br />
+        <label htmlFor="description">Description</label>
+        <input id="description" onInput={this.handleDescriptionInput}></input>
+        <br />
+        <input type="submit" />
+      </form>
     </>
   }
 }
